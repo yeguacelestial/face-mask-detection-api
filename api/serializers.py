4a1f3d6f55361ccd2027e49caf5bc3958da87f66
Yegua Celestial <yeguacelestial@gmail.com>
2021-04-21 17:32:25 -0500

    Get file directory from uploaded images

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from api.models import MaskDetector
 
 class MaskDetectorSerializer(serializers.ModelSerializer):
 
-    image = serializers.ImageField(max_length=None, use_url=True)
+    image = serializers.ImageField(max_length=None, use_url=False)
 
     class Meta:
         model = MaskDetector
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import os
+
 from rest_framework import views, status, viewsets, filters
 from rest_framework.response import Response
 
@@ -5,6 +7,22 @@ from api.models import MaskDetector
 
 from api.serializers import MaskDetectorSerializer
 
+from django.conf import settings
+
 class MaskDetectorViewSet(viewsets.ModelViewSet):
     queryset = MaskDetector.objects.all()
     serializer_class = MaskDetectorSerializer
+
+    def create(self, request):
+        serializer = MaskDetectorSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            image_dir = os.path.join(settings.MEDIA_ROOT, 'Images', str(request.data['image']))
+            print(f"IMAGE DIR => {image_dir}")
+            print(f"EXISTS? => {os.path.exists(image_dir)}")
+
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
